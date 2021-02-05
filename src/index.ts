578de83be46c2ecf30f2b8076322ba59848c8663
YoshikiMaruya <yoshimaru0514@gmail.com>
2021-02-05 09:25:34 +0900

    study types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-import World from './world'
+// import World from './world'
 
-const root = document.getElementById('root')
-const world = new World('Hello world')
-world.sayHello(root)
+// const root = document.getElementById( 'root' )
+// const world = new World('Hello Yoshiki')
+// world.sayHello(root)
+
+// 基本の型定義
+import { notExistSample, primitiveSample } from './basic'
+
+primitiveSample()
+notExistSample()
