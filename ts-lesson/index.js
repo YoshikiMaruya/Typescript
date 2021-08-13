e0be340601ebe6f0de94c2c2c9561249df1170ce
YoshikiMaruya <yoshimaru0514@gmail.com>
2021-08-13 22:12:03 +0900

    changed the code!

--- ts-lesson/index.js
+++ ts-lesson/index.js
@@ -1,2 +1,9 @@
 var hello = 'hello';
 console.log(hello);
+function add(a, b) {
+    return a + b;
+}
+function multi(a, b) {
+    return a * b;
+}
+console.log(multi(2, 3));
--- ts-lesson/index.ts
+++ ts-lesson/index.ts
@@ -4,3 +4,9 @@ console.log(hello);
 function add(a: number, b: number): number {
   return a + b;
 }
+
+function multi(a: number, b: number): number {
+  return a * b;
+}
+
+console.log(multi(2, 3));
